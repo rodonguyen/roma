36cec39df03e18efa72f1d4a33a97ea7e4e41d00
Rodo <rodonguyendd@gmail.com>
2022-08-02 21:27:08 +1000

    Add patient.js

---
+++ patient.js
@@ -0,0 +1,23 @@
+const axios = require('axios')
+
+const BASE_URL = 'http://nprogram.azurewebsites.net/'
+
+
+
+const queryPatient = async (patientID, format='json') => {
+    const fullUrl = `${BASE_URL}Patient/${patientID}?_format=${format}`
+    const response = await axios.get(fullUrl)
+    const data = response.data
+
+    console.log(data.name)
+    // return response
+}
+
+queryPatient(1)
+queryPatient(2)
+queryPatient(3)
+queryPatient(10)
+queryPatient(100)
+
+// const data = queryPatient(2)
+// console.log(data)
